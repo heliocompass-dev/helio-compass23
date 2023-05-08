d628fc4bcaa34f7b88dd0fb226346223dc23911c
yoshi6318 <y0sh1.5.zv@gmail.com>
2023-05-08 15:44:16 +0900

    add sns tumbnail setting

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&family=Noto+Serif+JP:wght@200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+
+          {/* Open Graph */}
+          <meta property="og:title" content="地球暦 HELIO COMPASS Official Website" />
+          <meta property="og:description" content="太陽系時空間地図 地球暦 / HELIO COMPASS は、太陽を中心とした地球の一年の動きを見ることができる暦です。" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://heliostera.com/" />
+          <meta property="og:image" content="https://heliostera.com/sns-thumbnail.jpg" />
+          <meta property="og:image:width" content="1366" />
+          <meta property="og:image:height" content="1040" />
+
+          {/* Twitter Card */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="地球暦 HELIO COMPASS Official Website" />
+          <meta name="twitter:description" content="太陽系時空間地図 地球暦 / HELIO COMPASS は、太陽を中心とした地球の一年の動きを見ることができる暦です。" />
+          <meta name="twitter:url" content="https://heliostera.com/" />
+          <meta name="twitter:image" content="https://heliostera.com/sns-thumbnail.jpg" />
         </Head>
+
         <body>
           <Main />
           <NextScript />
